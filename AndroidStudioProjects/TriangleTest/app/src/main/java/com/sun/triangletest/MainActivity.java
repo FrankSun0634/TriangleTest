318dc6e942d655598007ccdaba89e366fbe066e6
sunshiming <sunshiming0634@qq.com>
2015-08-13 22:10:36 +0800

    opengl es 绘制三角形

---
+++ AndroidStudioProjects/TriangleTest/app/src/main/java/com/sun/triangletest/MainActivity.java
@@ -0,0 +1,59 @@
+package com.sun.triangletest;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    public GLSurfaceView glView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        glView = new GLSurfaceView(this);
+        glView.setRenderer(new SimpleRender());
+        setContentView(glView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        glView.onResume();
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        glView.onPause();
+    }
+}
